05922d2b6df2f8515aa1a22f57cea6f7bbae7dc7
Thiago Geisler <contato.thiagogeisler@gmail.com>
2022-02-14 17:32:17 -0300

    Tópico: 07.Completando o CRUD.Subtópico:Formulário de alteração.

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -41,4 +41,13 @@ public class Banco {
 		}
 
 	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
 }
